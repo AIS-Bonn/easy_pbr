8811bb9c963895d99602798a7f472b11de7a41f3
Radu Alexandru Rosu <alejandro.rosu@gmail.com>
2019-11-20 20:11:10 +0000

    Made script to render the kitchen dataset together with labels and legend

---
+++ python/kitchen.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3.6
+
+import sys
+import os
+easy_pbr_path= os.path.join( os.path.dirname( os.path.realpath(__file__) ) , '../build')
+sys.path.append( easy_pbr_path )
+from EasyPBR  import *
+
+config_file="empty.cfg"
+
+# cloud=Mesh("/media/rosu/Data/data/ais_kitchen/kitchen_meshes/kitchen_gall.ply")
+cloud=Mesh("/media/rosu/Data/data/ais_kitchen/kitchen_meshes/kitchen_wrobel.ply")
+cloud.random_subsample(0.8)
+cloud.rotate_x_axis(180)
+cloud.m_vis.m_show_mesh=False
+cloud.m_vis.m_show_points=True
+
+# pred=Mesh("/media/rosu/Data/data/ais_kitchen/predictions/scannet_trained_after_submission_fixed_pointnet/0gall_pred.ply")
+pred=Mesh("/media/rosu/Data/data/ais_kitchen/predictions/scannet_trained_after_submission_fixed_pointnet/0wrobel_pred.ply")
+pred.m_vis.m_show_mesh=False
+pred.m_vis.m_show_points=True
+
+labels_file="/media/rosu/Data/data/scannet/colorscheme_and_labels/labels.txt"
+colorscheme_file="/media/rosu/Data/data/scannet/colorscheme_and_labels/color_scheme.txt"
+frequency_file="/media/rosu/Data/data/scannet/colorscheme_and_labels/frequency_uniform.txt"
+idx_unlabeled=0
+label_mngr=LabelMngr(labels_file, colorscheme_file, frequency_file, idx_unlabeled )
+label_mngr.compact("UNUSED")
+
+pred.m_label_mngr=label_mngr
+cloud.m_min_max_y_for_plotting=pred.m_min_max_y_for_plotting
+
+Scene.show(pred,"pred")
+Scene.show(cloud,"cloud")
+
+
+view=Viewer.create(config_file) #first because it needs to init context
+while True:
+    view.update()
+
+
