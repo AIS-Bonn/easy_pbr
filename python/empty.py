ebf4ac3c6d70ec25e91d5b774f5cf086e1d4cd2f
Radu Alexandru Rosu <alejandro.rosu@gmail.com>
2019-11-23 18:00:07 +0000

    Better detection of the python version

--- python/empty.py
+++ python/empty.py
@@ -2,9 +2,9 @@
 
 import sys
 import os
-# easy_pbr_path= os.path.join( os.path.dirname( os.path.realpath(__file__) ) , '../build')
+easy_pbr_path= os.path.join( os.path.dirname( os.path.realpath(__file__) ) , '../build')
 # easy_pbr_path= "/home/rosu/Downloads/conda/lib/python3.7/site-packages/lib/"
-easy_pbr_path= "/opt/conda/envs/pt/lib/"
+# easy_pbr_path= "/opt/conda/envs/pt/lib/"
 sys.path.append( easy_pbr_path )
 from EasyPBR  import *
 
