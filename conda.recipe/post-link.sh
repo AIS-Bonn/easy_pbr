6efa0b88b4e7f0298119922ee2dfa2314850407a
Radu Alexandru Rosu <alejandro.rosu@gmail.com>
2019-12-02 10:15:13 +0000

    Added more meshes on the kitchen dataset and a bit more verbose post link

--- conda.recipe/post-link.sh
+++ conda.recipe/post-link.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 
-# echo POST----------------------------------------------------------------------------- > $PREFIX/.messages.txt
+echo POST----------------------------------------------------------------------------- > $PREFIX/.messages.txt
 echo  post_link_copy > $PREFIX/.messages.txt
 # echo $PY_VER  >> $PREFIX/.messages.txt
 # cd $PREFIX/lib/python${PY_VER}/site-packages/
 # cd $PREFIX/lib/python3.8/site-packages
 cd $PREFIX/lib
 # ls >> $PREFIX/.messages.txt
-# find ./ -name "libeasypbr_cpp.so" >> $PREFIX/.messages.txt
+find ./ -name "libeasypbr_cpp.so" >> $PREFIX/.messages.txt
 EASYPBR_CPP_PATH=$(find ./ -name libeasypbr_cpp.so)
 NR_FINDS=${find ./ -name libeasypbr_cpp.so | grep . -c}
 # echo $EASYPBR_CPP_PATH>> $PREFIX/.messages.txt
 if [ $NR_FINDS -eq 1 ]
     then
+        echo "MOVING"  >> $PREFIX/.messages.txt
         mv "$EASYPBR_CPP_PATH" $PREFIX/lib
 fi
 # pwd >> $PREFIX/.messages.txt
--- python/kitchen.py
+++ python/kitchen.py
@@ -4,19 +4,23 @@ import sys
 import os
 easy_pbr_path= os.path.join( os.path.dirname( os.path.realpath(__file__) ) , '../build')
 sys.path.append( easy_pbr_path )
-from EasyPBR  import *
+from easypbr  import *
 
 config_file="empty.cfg"
 
 # cloud=Mesh("/media/rosu/Data/data/ais_kitchen/kitchen_meshes/kitchen_gall.ply")
-cloud=Mesh("/media/rosu/Data/data/ais_kitchen/kitchen_meshes/kitchen_wrobel.ply")
+# cloud=Mesh("/media/rosu/Data/data/ais_kitchen/kitchen_meshes/kitchen_wrobel.ply")
+# cloud=Mesh("/media/rosu/Data/data/ais_kitchen/kitchen_meshes/kitchen_ais.ply")
+cloud=Mesh("/media/rosu/Data/data/ais_kitchen/kitchen_meshes/kitchen_2105.ply")
 cloud.random_subsample(0.8)
 cloud.rotate_x_axis(180)
 cloud.m_vis.m_show_mesh=False
 cloud.m_vis.m_show_points=True
 
 # pred=Mesh("/media/rosu/Data/data/ais_kitchen/predictions/scannet_trained_after_submission_fixed_pointnet/0gall_pred.ply")
-pred=Mesh("/media/rosu/Data/data/ais_kitchen/predictions/scannet_trained_after_submission_fixed_pointnet/0wrobel_pred.ply")
+# pred=Mesh("/media/rosu/Data/data/ais_kitchen/predictions/scannet_trained_after_submission_fixed_pointnet/0wrobel_pred.ply")
+# pred=Mesh("/media/rosu/Data/data/ais_kitchen/predictions/scannet_trained_after_submission_fixed_pointnet/0ais_pred.ply")
+pred=Mesh("/media/rosu/Data/data/ais_kitchen/predictions/scannet_trained_after_submission_fixed_pointnet/0_2105_pred.ply")
 pred.m_vis.m_show_mesh=False
 pred.m_vis.m_show_points=True
 
