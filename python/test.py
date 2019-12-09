30cba7e9f16f7ee5eebbac049c5b996a24fcb2ce
Radu Alexandru Rosu <alejandro.rosu@gmail.com>
2019-12-09 21:12:32 +0000

    Cleanup test script

--- python/test.py
+++ python/test.py
@@ -1,24 +1,15 @@
 #!/usr/bin/env python3.6
 
 # import sys
-# import os
-# easy_pbr_path= os.path.join( os.path.dirname( os.path.realpath(__file__) ) , '../build')
-# easy_pbr_path= "/home/rosu/Downloads/conda/lib/python3.7/site-packages/lib/"
-# easy_pbr_path= "/opt/conda/envs/pt/lib/"
-# easy_pbr_path= "/tmp/tmpwpby4mtf/lib/"
-# sys.path.append( easy_pbr_path )
-
-# from distutils.sysconfig import get_python_inc
-# print( get_python_inc() ) #this gives the include dir
-
-
+import os
 from easypbr  import *
 
-# config_file="/media/rosu/Data/phd/c_ws/src/easy_pbr/config/empty.cfg"
-config_file="/media/rosu/Data/phd/c_ws/src/easy_pbr/config/empty.cfg"
+
+config_file="empty.cfg"
 
 
-view=Viewer.create(config_file) #first because it needs to init context
+config_path=os.path.join( os.path.dirname( os.path.realpath(__file__) ) , '../config', config_file)
+view=Viewer.create(config_path) #first because it needs to init context
 while True:
     view.update()
 
