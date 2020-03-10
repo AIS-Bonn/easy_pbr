14cba5f12626de1b02bf0601b42edba6ee6cd30d
Radu Alexandru Rosu <alejandro.rosu@gmail.com>
2020-03-10 12:32:44 +0000

    The CPP example reads the correct config

--- examples/example_cpp/src/main.cxx
+++ examples/example_cpp/src/main.cxx
@@ -2,13 +2,15 @@
 #include "easy_pbr/Viewer.h"
 #include "example_cpp/EasyPBRwrapper.h"
 
+//boost
+#include <boost/filesystem.hpp>
+namespace fs = boost::filesystem;
 
 int main(int argc, char *argv[]) {
 
-    std::string config_file="./config/example.cfg";
+    std::string config_file= fs::canonical(fs::path(PROJECT_SOURCE_DIR) / "./config/example.cfg").string();
 
-    std::shared_ptr<Viewer> view = Viewer::create();  //creates it with default params
-
+    std::shared_ptr<Viewer> view = Viewer::create(config_file);  //creates it with default params
     std::shared_ptr<EasyPBRwrapper> wrapper = EasyPBRwrapper::create(config_file, view);
 
     while (true) {
